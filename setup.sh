08315301f4c9d01541ad74427ff63f4fa8a24e1b
Your Name <you@example.com>
2024-10-05 10:35:32 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# 1. Copy vimrc file
+if [ -e ~/.vimrc ]; then
+    echo "~/.vimrc file already exists, exiting."
+    exit 1
+else
+    echo "1. Copying vimrc file."
+    cp ./vimrc ~/.vimrc 
+fi
+
+# 2. Setup colorscheme
+mkdir -p ~/.vim/colors
+echo "2. Downloading colorscheme.."
+curl -s -o ~/.vim/colors/toast.vim https://raw.githubusercontent.com/jsit/toast.vim/master/colors/toast.vim 
+
+# 3. Download plugin manager
+echo "3. Downloading vim-plug.."
+curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && echo 
+
+
+# 4. Install plugins
+echo "4. Installing plugins.."
+vim +PlugInstall +qall 
+
+echo "Setup complete!"
